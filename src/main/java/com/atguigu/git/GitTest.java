77d2e93afc03a4387c367d6168ad75a80079cee7
gengyiyang <gyy50760886@163.com>
2022-04-17 09:56:33 +0800

    push commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hello git123");
         System.out.println("hello git456");
         System.out.println("push test");
+        System.out.println("push test2");
     }
 }
